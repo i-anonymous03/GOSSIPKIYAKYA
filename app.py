c08a8d45e8124bd358c3f4205d5445159c8ca515
Prabhat <81768571+i-anonymous03@users.noreply.github.com>
2025-05-03 18:53:42 +0000

    update

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-# Flask app entry point
+from models import User
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-from flask import Flask
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/users.db'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+db = SQLAlchemy(app)
 
-@app.route('/')
-def home():
-    return 'Welcome to Gossip Kiya Kya!'
+
+# Step 1 route
+
+@app.route('/', methods=['GET', 'POST'])
+def step1():
+    if request.method == 'POST':
+        # Save data to session or db temporarily
+        return redirect(url_for('step2'))
+    return render_template('step1.html')
+
+# (More steps will be added later...)
+
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
